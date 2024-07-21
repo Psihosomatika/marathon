46ecb9747fc54ef1564771c62f57f14f9f5bd32e
Diana Rukavishnikova <psihomatika@gmail.com>
2024-07-21 19:18:43 +0200

    add

---
+++ hover/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#FFFFCC', '#FFCC99', '#CC9999', '#CC99CC', '#6699FF', '#99CCCC', '#99FF99', '#CCCCCC', '#FFFFFF']
+const SQUARES_NUMBER = 1482
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseleave', () => removeColor(square))
+
+  board.append(square)
+}
+
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d'
+  element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
